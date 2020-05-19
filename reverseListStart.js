ee7e46675ea8398ac20bd815167e796001e05695
Austin Wood <44730393+Awood549@users.noreply.github.com>
2020-05-18 22:18:27 -0700

    Create reverseListStart.js

---
+++ reverseListStart.js
@@ -0,0 +1,12 @@
+var reverseList = function(head) {
+    //instanciate a stack
+    //fill the stack with the LL contents 
+    //point the head to the tail and refill the LL
+    let current = head;
+    let temp;
+    
+    while(current.next){
+        arr.push(current)
+    }
+    
+};
